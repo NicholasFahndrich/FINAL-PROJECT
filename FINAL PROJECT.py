ec45e05672b73ae68a39e591118ab7679b1cd807
Nfahndrich <02nicholas.fahndrich@gulllakecs.org>
2019-03-11 09:59:53 -0700

    Second Commit

---
+++ FINAL PROJECT.py
@@ -0,0 +1,12 @@
+# Nicholas Fahndrich
+# 3/11/19
+# FINAL PROJECT
+
+name = input("What is your name: ")
+
+def greeting():
+    print("Hi there " + name + "! ")
+    print("Welcome to the weight calculator")
+
+
+question = int(input(("How long have you been lifting"))
