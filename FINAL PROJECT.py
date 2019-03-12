c1b0bb3a23f1d9ff41e1844c12fed16256ec6ff9
Nfahndrich <02nicholas.fahndrich@gulllakecs.org>
2019-03-12 10:00:08 -0700

    Added Test Master

--- FINAL PROJECT.py
+++ FINAL PROJECT.py
@@ -8,13 +8,13 @@ print("Hi there " + name + "! " )
 print("Welcome to the weight calculator" + "\n")
 
 while True:
-    age = int(input("Enter An age:"))
+    age = int(input("Enter your age:"))
     if age > 100:
         print("You are too old to be lifting" + "\n")
     elif age < 14:
         print("You are too young to lift", "\n")
         break
     else:
-        print("Thats the perfect age", "\n")
+        print("Thats the perfect age to be lifting", "\n")
         break
 
